95c699dc86115b62a7a9e2668a372e3bef4ae3fa
Gustav Joachim Elbrønd <202006022@post.au.dk>
2022-02-03 13:27:52 +0100

    Mit navn

--- SWT_Gr8_Opg1/Program.cs
+++ SWT_Gr8_Opg1/Program.cs
@@ -9,6 +9,7 @@ namespace SWT_Gr8_Opg1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Mojn");
             Console.WriteLine("katrine");
+            Console.WriteLine("Gustav");
         }
     }
 }
