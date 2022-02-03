ac796708e6b1e062da5190324a7a65f80b9021ae
NadiaAJensen <78858892+NadiaAJensen@users.noreply.github.com>
2022-02-03 13:12:07 +0100

    Add project files.

---
+++ SWT_Gr8_Opg1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWT_Gr8_Opg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
